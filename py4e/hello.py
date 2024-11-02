31124a14cbc5c84136e265e01a1961c20a073cde
jhin93 <scene1993@gmail.com>
2024-11-02 19:09:11 +0900

    assignment 2

--- py4e/hello.py
+++ py4e/hello.py
@@ -1,10 +1,15 @@
-rawstr = input('Enter a number:')
-try:
-  ival = int(rawstr)
-except:
-  ival = -1
-
-if ival > 0 : 
-  print('Nice work')
-else : 
-  print('Not a number')
+score = input("Enter Score: ")
+fscore = float(score)
+if fscore > 1 or fscore < 0:
+    print('out of range')
+else:
+    if fscore >= 0.9:
+        print('A')
+    elif fscore >= 0.8:
+        print('B')
+    elif fscore >= 0.7:
+        print('C')
+    elif fscore >= 0.6:
+        print('D')
+    elif fscore < 0.6:
+        print('F')
