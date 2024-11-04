ee69d44a187d7aae353343cf018c974955c7f7bf
jhin93 <scene1993@gmail.com>
2024-11-04 20:41:54 +0900

    parameters

--- README.md
+++ README.md
@@ -252,3 +252,17 @@ print(tiny)
 # 10
 # 7
 ```
+<!-- Parameters -->
+```python
+def greet(lang):
+    if lang == 'es':
+        print('Hola')
+    elif lang == 'fr':
+        print('Bonjour')
+    else:
+        print('Hello')
+
+greet('en') # Hello
+greet('es') # Hola
+greet('fr') # Boungjour
+```
--- py4e/hello.py
+++ py4e/hello.py
@@ -1,5 +1,11 @@
-big = max([10,9,8,7])
-print(big)
+def greet(lang):
+    if lang == 'es':
+        print('Hola')
+    elif lang == 'fr':
+        print('Bonjour')
+    else:
+        print('Hello')
 
-tiny = min([10,9,8,7])
-print(tiny)
+greet('en') # Hello
+greet('es') # Hola
+greet('fr') # Boungjour
