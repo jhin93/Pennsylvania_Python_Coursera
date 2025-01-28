2cbb1ee06761aabfcfd99274e273f9e51e11d164
jhin93 <scene1993@gmail.com>
2025-01-28 15:21:02 +1100

    Writing comment with cursor

--- py4e/hello.py
+++ py4e/hello.py
@@ -1,3 +1,8 @@
+# Create a list of friends' names
 friends = ['sally', 'glenn', 'joseph']
+
+# Sort the list alphabetically 
 friends.sort()
-print(friends)
+
+# Print the sorted list of friends
+print(friends)  # Output: ['glenn', 'joseph', 'sally']
