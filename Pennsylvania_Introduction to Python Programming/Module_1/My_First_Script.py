a42801bfddcd61409819be6fe72b931de5bad75d
jhin93 <scene1993@gmail.com>
2025-02-23 19:40:08 +1100

    input

--- Pennsylvania_Introduction to Python Programming/Module_1/My_First_Script.py
+++ Pennsylvania_Introduction to Python Programming/Module_1/My_First_Script.py
@@ -10,3 +10,11 @@ print('four')
 """here's a
 multi-line comment"""
 
+"""
+This code will:
+Prompt for your name
+Set the 'name' variable
+Print a message
+"""
+name = input("What is your name?") #gets the user's name
+print("Hello " + name) #prints a message
