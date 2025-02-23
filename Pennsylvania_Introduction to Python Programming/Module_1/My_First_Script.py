99d99ef719ec16abd7a4273a169e5d101b14fc0c
jhin93 <scene1993@gmail.com>
2025-02-23 19:49:17 +1100

    str * 2 == strstr

--- Pennsylvania_Introduction to Python Programming/Module_1/My_First_Script.py
+++ Pennsylvania_Introduction to Python Programming/Module_1/My_First_Script.py
@@ -1,24 +1,25 @@
 #Here are my print statement
 
-print('one')
-print('two')
-print('three')
-print('four')
+x = y = 10
+z = 2 * x + y
+print(z)
 
 #Here's a single line comment
 
 """here's a
 multi-line comment"""
 
-"""
-This code will:
-Prompt for your name
-Set the 'name' variable
-Print a message
-"""
-name = input("What is your name?") #gets the user's name
-print("Hello " + name) #prints a message
+z = z ** (y - 1)
+print(z)
 
-testTrue = True
-print(not testTrue)
+b = 15 < (x / 2) < 25
+print(b)
+print(type(b))
+
+x = 42
+y = str(x)
+x *= 2
+y *= 2
+print(x) #84
+print(y) #'4242'
 
