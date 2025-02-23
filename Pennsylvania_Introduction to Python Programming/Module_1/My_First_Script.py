bfedb43b36d7dd19d9db0f0b9e24d299a51e31cb
jhin93 <scene1993@gmail.com>
2025-02-23 20:07:29 +1100

    total bill tax tip calculate

--- Pennsylvania_Introduction to Python Programming/Module_1/My_First_Script.py
+++ Pennsylvania_Introduction to Python Programming/Module_1/My_First_Script.py
@@ -1,4 +1,15 @@
-age = int(input("How old are you?"))
-print(type(age))
 
-print(age + 3)
+bill = float(input("How much is the meal?"))
+tax = float(input("What is the sales tax(in %)?"))
+tip = float(input("How much tip do you want to give(in %)?"))
+
+tax_amount = (bill * tax) / 100
+total = bill + tax_amount
+tip_amount = (total * tip) / 100
+
+total = total + tip_amount
+
+#round the total to 2 decimal places
+total = round(total, 2)
+
+print("The total bill is $", total, sep = '')
