26884c68fa1af90fac6988b9513e279bc4b0e9c4
jhin93 <scene1993@gmail.com>
2025-03-03 11:32:15 +1100

    중간커밋

--- Pennsylvania_Introduction to Python Programming/Module_2/DataAnnotation/Assessment.py
+++ Pennsylvania_Introduction to Python Programming/Module_2/DataAnnotation/Assessment.py
@@ -7,26 +7,12 @@ url = "https://docs.google.com/document/d/e/2PACX-1vRMx5YQlZNa3ra8dYYxmv-QIQ3YJe
 def extractUrlData(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    table = soup.find_all('tr', class_='c4')
-    dataArr = []
-    skip_count = 0
-    for row in table:
-        cells = row.find_all(['td'])
-        for cell in cells:
-            if cell.text.strip():
-                if skip_count < 3:
-                    skip_count += 1
-                else:
-                    dataArr.append(cell.text.strip())
-                print(dataArr)
-    print("123123123")
-    resultArr = []
-    for i in range(len(dataArr), 3):
-        if i + 2 < len(dataArr):
-            group_data = [dataArr[i], dataArr[i+1], dataArr[i+2]]
-            resultArr.append(group_data)
-    print(resultArr)
-    return resultArr
+    spanData = soup.select('table span')
+    tagData =  spanData[3:]
+    print("======================== tagData ========================", tagData)
+    
+    return tagData
+
 
 
 extractUrlData(url)
