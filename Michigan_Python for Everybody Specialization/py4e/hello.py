9c2755323e8e2f35823d1a6fcf88d5253acdf59d
jhin93 <scene1993@gmail.com>
2025-03-11 13:57:17 +1100

    move michigan to somewhere

--- Michigan_Python for Everybody Specialization/py4e/hello.py
+++
@@ -1,8 +0,0 @@
-# Create a list of friends' names
-friends = ['sally', 'glenn', 'joseph']
-
-# Sort the list alphabetically 
-friends.sort()
-
-# Print the sorted list of friends
-print(friends)  # Output: ['glenn', 'joseph', 'sally']
