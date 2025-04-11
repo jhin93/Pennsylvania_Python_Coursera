91d5d47188e4a7050baee17df07500af978d4f9a
jhin93 <scene1993@gmail.com>
2025-04-11 15:04:22 +1000

    step1

--- UTS_semester1--Fundamental_programming/Lab8/Lab8Exc1.py
+++ UTS_semester1--Fundamental_programming/Lab8/Lab8Exc1.py
@@ -1,14 +1,15 @@
-import random
+from abc import ABC, abstractmethod
 
 # Step 1: Develop the Polygon class
 # Polygon has one field: type
 # Polygon has one constructor that initializes the field "type" from parameter
 # Polygon has an abstract method 'area()' with no parameters and returns a double
 
-class Polygon:
+class Polygon(ABC):
     def __init__(self, type):
         self.type = type
 
+    @abstractmethod
     def area(self):
         pass
 
