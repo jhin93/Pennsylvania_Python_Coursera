f487a09eb5cac70050329cc7c9d411ab54001504
jhin93 <scene1993@gmail.com>
2025-04-11 14:55:00 +1000

    Lab8Exc1

---
+++ UTS_semester1--Fundamental_programming/Lab8/Lab8Exc1.py
@@ -0,0 +1,14 @@
+import random
+
+# Step 1: Develop the Polygon class
+# Polygon has one field: type
+# Polygon has one constructor that initializes the field "type" from parameter
+# Polygon has an abstract method 'area()' with no parameters and returns a double
+
+class Polygon:
+    def __init__(self, type):
+        self.type = type
+
+    def area(self):
+        pass
+
