d332b9073b16a48375f2ebd0d81b3afe54e6b269
jhin93 <scene1993@gmail.com>
2025-04-11 15:15:26 +1000

    step 2

--- UTS_semester1--Fundamental_programming/Lab8/Lab8Exc1.py
+++ UTS_semester1--Fundamental_programming/Lab8/Lab8Exc1.py
@@ -13,3 +13,24 @@ class Polygon(ABC):
     def area(self):
         pass
 
+    def __str__(self):
+        return f"{self.type}"
+
+# Step 2: Develop the class Square
+# Square is subclass of Polygon
+# Square has one field: side of type double
+# Square has one constructor that initializes the field side from parameter
+# Square overrides the method 'area' defined in Polygon to return the area of square
+# Square overrides the toString() method to return: "<type> area = <area>"
+
+class Square(Polygon):
+    def __init__(self, side):
+        super().__init__("Square")
+        self.side = side
+
+    def area(self):
+        return self.side * self.side
+    
+    def __str__(self):
+        return f"{self.type} area = {self.area()}"
+    
