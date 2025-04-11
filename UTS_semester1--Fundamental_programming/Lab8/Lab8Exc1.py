c65df122a5f06aa92296938c4378817ad00bae5f
jhin93 <scene1993@gmail.com>
2025-04-11 15:49:20 +1000

    Exercise1 complete

--- UTS_semester1--Fundamental_programming/Lab8/Lab8Exc1.py
+++ UTS_semester1--Fundamental_programming/Lab8/Lab8Exc1.py
@@ -53,3 +53,30 @@ class Triangle(Polygon):
     def __str__(self):
         return f"{self.type} area = {self.area()}"
     
+# Step 4: Develop the class Shapes
+# Shape has a field 'shapes' of type List which stores Polygon objects
+# Shape has one constructor that initializes the field shapes
+# Shape has a method 'loadShapes' to populate the List with the following objects:
+# - 2 Square objects with sides of 5 and 10
+# - 2 Triangle objects with height of 5 and base of 3, and height of 5 and base of 5
+# Shape has a method 'show' that prints out information about all its Polygon objects
+# Shape has the main() method that creates an instance of Shapes and invoke the method 'show'
+
+class Shapes:
+    def __init__(self):
+        self.shapes = []
+
+    def loadShapes(self):
+        self.shapes.append(Square(5))
+        self.shapes.append(Square(10))
+        self.shapes.append(Triangle(5, 3))
+        self.shapes.append(Triangle(5, 5))
+
+    def show(self):
+        for shape in self.shapes:
+            print(shape)
+
+if __name__ == "__main__":
+    shapes = Shapes()
+    shapes.loadShapes()
+    shapes.show()
